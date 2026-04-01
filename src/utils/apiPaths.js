c6848685087b0e186f119c4587f458c710bd1f68
MohamedAbobakr12 <ziadabobakr@gmail.com>
2026-04-01 17:13:04 +0200

    update apiPaths.js and add .env in .gitignore

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expense-tracker-app-backend-phi.vercel.app";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // utils/apiPath.js
 export const API_PATHS = {
